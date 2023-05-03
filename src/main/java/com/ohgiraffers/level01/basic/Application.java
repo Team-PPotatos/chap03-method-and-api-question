fa9531a44efc99e3c4343bf964e7b482c52fa3ac
jinchiim <nellroll@naver.com>
2023-05-03 23:57:22 +0900

    [fix] level01 basic Application
    [fix] Calculator 수정에 따른 수정

--- src/main/java/com/ohgiraffers/level01/basic/Application.java
+++ src/main/java/com/ohgiraffers/level01/basic/Application.java
@@ -6,8 +6,8 @@ public class Application {
 
         Calculator calculator = new Calculator();
         calculator.checkMethod();
-        calculator.sum1to10();
-        System.out.println("두 수 중 큰 수는 " + calculator.checkMaxNumber(10, 20) + "이다.");
+        System.out.println("1부터 10까지의 합은 : " + calculator.sum1to10());
+        calculator.checkMaxNumber(10, 20);
         System.out.println("10과 20의 합은 : " + calculator.sumTwoNumber(10, 20));
         System.out.println("10과 5의 차는 : " + calculator.minusTwoNumber(10, 5));
     }
