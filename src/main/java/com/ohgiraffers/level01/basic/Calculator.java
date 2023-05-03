c6150a47236dac22ac77f9ccd80f08023bd981b9
jinchiim <nellroll@naver.com>
2023-05-03 17:13:55 +0900

    level01 basic Calculator init-commit
    level01 basic Calculator init-commit

---
+++ src/main/java/com/ohgiraffers/level01/basic/Calculator.java
@@ -0,0 +1,27 @@
+package com.ohgiraffers.level01.basic;
+
+public class Calculator {
+    public void checkMethod(){
+        System.out.println("메소드 호출 확인");
+    }
+    public int sum1to10(){
+        int sum = 0;
+        for (int i=1; i<11; i++){
+            sum += i;
+        };
+        return sum;
+    }
+
+    public int checkMaxNumber(int a, int b) {
+        return (a > b)? a : b;
+    }
+
+    public int sumTwoNumber(int a, int b){
+        return a+b;
+    }
+
+    public int minusTwoNumber(int a, int b){
+        return a-b;
+    }
+
+}
