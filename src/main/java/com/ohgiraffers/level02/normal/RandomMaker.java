91c54be80d5386a4e003a81d6717f4dda85234a4
JUNE-LABTOP\김준열 <june5228@gmail.com>
2023-05-03 17:47:01 +0900

    RandomMaker code

---
+++ src/main/java/com/ohgiraffers/level02/normal/RandomMaker.java
@@ -0,0 +1,41 @@
+package com.ohgiraffers.level02.normal;
+import java.util.Random;
+public class RandomMaker {
+    public int randomNumber(int min, int max){
+        Random random = new Random();
+        int randomNumber = random.nextInt(max) + min;
+
+        return randomNumber;
+    }
+    public String randomUpperAlphabet(int length) {
+        Random random = new Random();
+        String result = "";
+        for (int i = 0; i < length; i++) {
+            result += String.valueOf((char) ((random.nextInt(26)) + 65));
+        }
+
+        return result;
+    }
+    public String rockPaperScissors(){
+        Random random = new Random();
+        String result = "";
+        int randomNumber = random.nextInt(3) + 1;
+        if(randomNumber == 1){
+            result += "Rock";
+        } else if (randomNumber == 2) {
+            result += "Paper";
+        } else result += "Scissors";
+
+        return result;
+    }
+    public String tossCoin(){
+        Random random = new Random();
+        String result = "";
+        int randomNumber = random.nextInt(2) + 1;
+        if(randomNumber == 1){
+            result += "앞면";
+        } else result += "뒷면";
+
+        return result;
+    }
+}
