d4117bf293063ae33afdf3435e5206d1a0aa6ecb
DESKTOP-KTEEVGU\hanme <2hanme527@naver.com>
2023-05-04 10:19:03 +0900

    [fix] normal 1. RandomMaker (method)
    static 메서드 호출 수정

--- src/main/java/com/ppotatos/level02/normal/Application.java
+++ src/main/java/com/ppotatos/level02/normal/Application.java
@@ -4,11 +4,9 @@ public class Application {
     public static void main(String[] args) {
 
         /* RandomMaker 클래스에 있는 각 메소드를 호출 */
-        RandomMaker rm = new RandomMaker();
-
-        System.out.println(rm.randomNumber(-50, 50));
-        System.out.println(rm.randomUpperAlphabet(20));
-        System.out.println(rm.rockPaperScissors());
-        System.out.println(rm.tossCoin());
+        System.out.println(RandomMaker.randomNumber(-50, 50));
+        System.out.println(RandomMaker.randomUpperAlphabet(20));
+        System.out.println(RandomMaker.rockPaperScissors());
+        System.out.println(RandomMaker.tossCoin());
     }
 }
